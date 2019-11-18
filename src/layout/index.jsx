1dbd92c83012407898ea7874844a0173f379963b
davesantos <info@davesantos.com>
2019-11-18 15:15:35 -0800

    Add index.css

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -3,6 +3,7 @@ import Helmet from "react-helmet";
 import { Link } from "gatsby";
 import config from "../../data/SiteConfig";
 import "normalize.css";
+import "./index.css";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
