1eba98abd729e0333011df5546b10ca85e90d063
davesantos <info@davesantos.com>
2019-11-18 15:09:08 -0800

    Add root import

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-root-import`,
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-lodash",
     {
